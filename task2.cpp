e6d780f310a99ca3ed66d270b632ea454b5105fe
annaZakharchenko <anna.zakharchenko@student.karazin.ua>
2025-04-24 11:49:10 +0200

    practice 8

--- task2.cpp
+++ task2.cpp
@@ -3,62 +3,60 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
+#include <chrono>
 
 using namespace std;
 
+mutex mtx;
+condition_variable cv;
 queue<int> buffer;
 const unsigned int MAX_BUFFER_SIZE = 5;
-
-mutex mtx;
-condition_variable cv_producer;
-condition_variable cv_consumer;
-
-bool finished = false;
+bool done = false;
 
 void producer() {
     for (int i = 1; i <= 10; ++i) {
         unique_lock<mutex> lock(mtx);
-        cv_producer.wait(lock, [] { return buffer.size() < MAX_BUFFER_SIZE; });
+        cv.wait(lock, [] { return buffer.size() < MAX_BUFFER_SIZE; });
 
         buffer.push(i);
         cout << "Produced: " << i << endl;
 
         lock.unlock();
-        cv_consumer.notify_one();
-        this_thread::sleep_for(chrono::milliseconds(100));
+        cv.notify_all();
+        this_thread::sleep_for(chrono::milliseconds(200));
     }
 
     unique_lock<mutex> lock(mtx);
-    finished = true;
-    cv_consumer.notify_all();
+    done = true;
+    lock.unlock();
+    cv.notify_all();
 }
 
 void consumer() {
     while (true) {
         unique_lock<mutex> lock(mtx);
-        cv_consumer.wait(lock, [] { return !buffer.empty() || finished; });
+        cv.wait(lock, [] { return !buffer.empty() || done; });
 
         while (!buffer.empty()) {
-            int val = buffer.front();
+            int value = buffer.front();
             buffer.pop();
-            cout << "Consumed: " << val << endl;
-
-            lock.unlock();
-            cv_producer.notify_one();
-            this_thread::sleep_for(chrono::milliseconds(150));
-            lock.lock();
+            cout << "Consumed: " << value << endl;
         }
 
-        if (finished) break;
+        if (done) break;
+
+        lock.unlock();
+        cv.notify_all();
+        this_thread::sleep_for(chrono::milliseconds(300));
     }
 }
 
 int main() {
-    thread t1(producer);
-    thread t2(consumer);
+    thread prod(producer);
+    thread cons(consumer);
 
-    t1.join();
-    t2.join();
+    prod.join();
+    cons.join();
 
     return 0;
 }
